cc069cf9f102997191373508f3d6eea138e86d21
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-05-03 18:15:35 -0700

    Compute fundamental Mobius triangle

--- src/wythoff.test.ts
+++ src/wythoff.test.ts
@@ -45,12 +45,12 @@ test("SchwarzTriangle.fromPoints idempotent on Mobius triangles", () => {
 
 test("Icosahedral group has order 120", () => {
   const triangle = new wythoff.SchwarzTriangle(2, 3, 5);
-  expect(wythoff.PointGroup.fromSchwarzTriangle(triangle).order).toBe(120);
+  expect(wythoff.SymmetryGroup.fromSchwarzTriangle(triangle).order).toBe(120);
 });
 
 test("4 | 2 3 (octahedron) has 6 vertices", () => {
   const triangle = new wythoff.SchwarzTriangle(4, 2, 3);
-  const polyhedron = wythoff.PointGroup.fromSchwarzTriangle(triangle).makePolyhedron(false);
+  const polyhedron = wythoff.SymmetryGroup.fromSchwarzTriangle(triangle).makePolyhedron(false);
   expect(polyhedron.vertices.length).toBe(6);
   expect(polyhedron.edges.length).toBe(12);
   expect(polyhedron.faces.length).toBe(8);
@@ -58,7 +58,7 @@ test("4 | 2 3 (octahedron) has 6 vertices", () => {
 
 test("5 | 2 3 (icosahedron) has 12 vertices", () => {
   const triangle = new wythoff.SchwarzTriangle(5, 2, 3);
-  const polyhedron = wythoff.PointGroup.fromSchwarzTriangle(triangle).makePolyhedron(false);
+  const polyhedron = wythoff.SymmetryGroup.fromSchwarzTriangle(triangle).makePolyhedron(false);
   expect(polyhedron.vertices.length).toBe(12);
   expect(polyhedron.edges.length).toBe(30);
   expect(polyhedron.faces.length).toBe(20);
@@ -66,7 +66,7 @@ test("5 | 2 3 (icosahedron) has 12 vertices", () => {
 
 test("3 | 2 5 (dodecahedron) has 20 vertices", () => {
   const triangle = new wythoff.SchwarzTriangle(3, 2, 5);
-  const polyhedron = wythoff.PointGroup.fromSchwarzTriangle(triangle).makePolyhedron(false);
+  const polyhedron = wythoff.SymmetryGroup.fromSchwarzTriangle(triangle).makePolyhedron(false);
   expect(polyhedron.vertices.length).toBe(20);
   expect(polyhedron.edges.length).toBe(30);
   expect(polyhedron.faces.length).toBe(12);
@@ -74,7 +74,7 @@ test("3 | 2 5 (dodecahedron) has 20 vertices", () => {
 
 test("2 | 3 5 (icosidodecahedron) has 30 vertices", () => {
   const triangle = new wythoff.SchwarzTriangle(2, 3, 5);
-  const polyhedron = wythoff.PointGroup.fromSchwarzTriangle(triangle).makePolyhedron(true);
+  const polyhedron = wythoff.SymmetryGroup.fromSchwarzTriangle(triangle).makePolyhedron(true);
   expect(polyhedron.vertices.length).toBe(30);
   expect(polyhedron.edges.length).toBe(60);
   expect(polyhedron.faces.length).toBe(32);
