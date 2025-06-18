b3ce79ca2216e8333233b59cf036b480075ebc25
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-06-17 19:52:47 -0700

    Reorg UI

--- src/App.vue
+++ src/App.vue
@@ -272,29 +272,6 @@ const cameraDirection = camera.direction;
 
       <div class="flex flex-row">
         <div class="flex flex-row gap-2 flex-1">
-          <Button
-            @click="camera.reset"
-            material
-            icon="home"
-            help="Reset camera"
-          />
-          <Button
-            @click="cameraControls.zoomIn"
-            material
-            icon="add"
-            help="Zoom in"
-          />
-          <div
-            class="size-8 py-1 -mx-1 text-center material text-gray-200 select-none"
-          >
-            search
-          </div>
-          <Button
-            @click="cameraControls.zoomOut"
-            material
-            icon="remove"
-            help="Zoom out"
-          />
         </div>
         <Selector />
         <div class="flex-1 flex flex-row justify-end gap-2">
@@ -303,12 +280,6 @@ const cameraDirection = camera.direction;
             material
             icon="casino" 
             help="Random"
-            />
-          <Button
-            @click="takeScreenshot"
-            material
-            icon="photo_camera"
-            help="Take screenshot"
             />
           <Button
             @click="toggleExperimentalMode"
@@ -338,6 +309,42 @@ const cameraDirection = camera.direction;
           <div class="w-25">
             <strong>Twisters:</strong> {{ polytwister.polyhedron.faces.length }}
           </div>
+        </div>
+      </div>
+
+      <div class="flex flex-row">
+        <div class="flex flex-row gap-2 flex-1">
+          <Button
+            @click="camera.reset"
+            material
+            icon="home"
+            help="Reset camera"
+          />
+          <Button
+            @click="cameraControls.zoomIn"
+            material
+            icon="add"
+            help="Zoom in"
+          />
+          <div
+            class="size-8 py-1 -mx-1 text-center material text-gray-200 select-none"
+          >
+            search
+          </div>
+          <Button
+            @click="cameraControls.zoomOut"
+            material
+            icon="remove"
+            help="Zoom out"
+          />
+        </div>
+        <div class="flex-1 flex flex-row justify-end gap-2">
+          <Button
+            @click="takeScreenshot"
+            material
+            icon="photo_camera"
+            help="Take screenshot"
+            />
         </div>
       </div>
 
