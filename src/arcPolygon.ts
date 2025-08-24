66d835543b644115e42e65a5f185f8014ae77e21
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-08-24 12:30:43 -0700

    Continue work on nonmonotonic bloated case

--- src/arcPolygon.ts
+++ src/arcPolygon.ts
@@ -2,13 +2,12 @@
  * In this module, an (n, r) circle configuration is a set of n circles whose centers are positioned
  * at the vertices of a regular n-gon and are all of distance 1 from the origin, and all their radii
  * are r.
- * 
+ *
  * If r starts from 0 and increases, the adjacency graph of region classes changes and is assigned
  * an index. Index 0 is a degenerate case where no circles intersect. Index Math.ceil(n / 2) is the
  * case where all circles contain the origin.
  */
 import { mod } from "./mathUtils";
-
 
 /**
  * Return the radii of an (n, r) circle configuration where r crosses a threshold that changes the
@@ -18,7 +17,7 @@ import { mod } from "./mathUtils";
 export function getCriticalRadii(n: number): number[] {
   const result = [];
   for (let q = 0; q < Math.ceil(n / 2); q++) {
-    const angle = q * 2 * Math.PI / n;
+    const angle = (q * 2 * Math.PI) / n;
     const radius = Math.hypot(1 - Math.cos(angle), Math.sin(angle)) / 2;
     result.push(radius);
   }
@@ -48,61 +47,69 @@ export function getRadiusIndex(n: number, r: number): number {
 export enum RegionMode {
   Inner,
   Outer,
-  Both
+  Both,
 }
 
 export interface Region {
-  order: number,
-  mode: RegionMode
+  order: number;
+  mode: RegionMode;
 }
 
 /**
  * Return the filled-in regions for a binary filling, given n, radius index q, rotation number d,
  * and bloatedness.
  */
-export function regions(n: number, q: number, d: number, bloated: boolean): Region[] {
+export function regions(
+  n: number,
+  q: number,
+  d: number,
+  bloated: boolean,
+): Region[] {
   const result: Region[] = [];
   if (q === Math.ceil(n / 2)) {
-    // Monotonic case. 
     if (!bloated) {
+      // Monotonic + nonbloated.
       for (let i = 0; i <= n; i++) {
         if (i > n - d && mod(i - (n - d), 2) === 1) {
           result.push({
             order: i,
             mode: RegionMode.Both,
-          })
+          });
         }
       }
     } else {
+      // Monotonic + bloated.
       for (let i = 0; i <= n; i++) {
         if ((i < d && mod(i, 2) === 1) || (mod(d, 2) === 1 && i >= d)) {
           result.push({
             order: i,
             mode: RegionMode.Both,
-          })
+          });
         }
       }
     }
   } else {
-    // Non-monotonic case.
     if (!bloated) {
       if (d < n / 2) {
+        // Non-monotonic + non-bloated + curve does not divide regions
+        // [q] and [q+1], so all regions are inner.
         for (let i = 0; i < q; i++) {
           if (i < d && mod(d - i, 2) === 1) {
             result.push({
               order: i,
               mode: RegionMode.Inner,
-            })
+            });
           }
         }
       } else {
+        // Non-monotonic + non-bloated + curve divides regions [q] and [q+1].
         const u = d - n + q;
         for (let i = 0; i < q; i++) {
           if (i > q - u && mod(i - (q - u), 2) === 1) {
             result.push({
               order: i,
               mode: RegionMode.Outer,
-            })
+            });
           }
         }
         const qFilled = mod(u, 2) === 1;
@@ -110,20 +117,33 @@ export function regions(n: number, q: number, d: number, bloated: boolean): Regi
           result.push({
             order: q,
             mode: RegionMode.Both,
-          })
+          });
         }
         if (!qFilled) {
           result.push({
             order: q + 1,
             mode: RegionMode.Both,
-          })
+          });
         }
         for (let i = 0; i < q; i++) {
           if ((mod(q - i, 2) === 0) !== !qFilled) {
             result.push({
               order: i,
               mode: RegionMode.Inner,
-            })
+            });
+          }
+        }
+      }
+    } else {
+      if (d < n / 2) {
+        // Non-monotonic + bloated + curve does not divide regions
+        // [q] and [q+1], so all inner regions are the same color.
+        for (let i = 0; i < q; i++) {
+          if (i < d && mod(d - i, 2) === 1) {
+            result.push({
+              order: i,
+              mode: RegionMode.Inner,
+            });
           }
         }
       }
@@ -139,10 +159,16 @@ export function regionsToString(regions: Region[]): string {
     if (region1.mode === RegionMode.Both && region2.mode === RegionMode.Both) {
       return region2.order - region1.order;
     }
-    if (region1.mode === RegionMode.Inner && region2.mode === RegionMode.Inner) {
+    if (
+      region1.mode === RegionMode.Inner &&
+      region2.mode === RegionMode.Inner
+    ) {
       return region1.order - region2.order;
     }
-    if (region1.mode === RegionMode.Outer && region2.mode === RegionMode.Outer) {
+    if (
+      region1.mode === RegionMode.Outer &&
+      region2.mode === RegionMode.Outer
+    ) {
       return region2.order - region1.order;
     }
     if (region1.mode === RegionMode.Inner) {
@@ -159,16 +185,16 @@ export function regionsToString(regions: Region[]): string {
     }
     return 0;
   });
-  const parts = sortedRegions.map((region: Region) => (
-    "[" +
-    region.order.toString() + (
-      region.mode === RegionMode.Both
-      ? ""
-      : region.mode === RegionMode.Inner
-      ? "-"
-      : "+"
-    )
-    + "]"
-  ));
+  const parts = sortedRegions.map(
+    (region: Region) =>
+      "[" +
+      region.order.toString() +
+      (region.mode === RegionMode.Both
+        ? ""
+        : region.mode === RegionMode.Inner
+          ? "-"
+          : "+") +
+      "]",
+  );
   return parts.join(" ");
 }
