c468278c48ceb17e7abbea47c7948cb5e08218ea
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-10-08 19:36:28 -0700

    Wrap WSlider in a div

--- src/WSlider.vue
+++ src/WSlider.vue
@@ -101,65 +101,67 @@ function setPlayMode(newPlayMode: PlayMode) {
 </script>
 
 <template>
-  <div class="relative">
-    <input
-      type="range"
-      class="w-full"
-      v-model.number="model"
-      min="-1.0"
-      max="1.0"
-      step="0.0001"
-      @pointerdown="pause"
-    />
-  </div>
-  <div class="flex flex-row w-full">
-    <div class="flex-1 flex justify-start items-center gap-2">
-      <div>Cross section <em>w</em></div>
-      <div>=</div>
+  <div>
+    <div class="relative">
       <input
-        class="bg-primary p-1 rounded-sm text-center"
-        type="number"
+        type="range"
+        class="w-full"
         v-model.number="model"
-        min="-1"
-        max="1"
-        step="0.01"
+        min="-1.0"
+        max="1.0"
+        step="0.0001"
+        @pointerdown="pause"
       />
     </div>
-    <div class="flex-1 flex justify-center items-center gap-2">
-      <Button @click="goToZero" square text="0" help="W = 0" />
-      <Button
-        :active="playing"
-        @click="togglePlay"
-        icon="play_arrow"
-        help="Play"
-      />
-      <Button @click="pause" icon="pause" help="Pause" />
-    </div>
-    <div class="flex-1 flex justify-end items-center gap-2">
-      <Button
-        @click="setPlayMode(PlayMode.Loop)"
-        :active="playMode === PlayMode.Loop"
-        icon="laps"
-        help="Loop: wrap"
-      />
-      <Button
-        @click="setPlayMode(PlayMode.Zigzag)"
-        :active="playMode === PlayMode.Zigzag"
-        icon="turn_sharp_right"
-        help="Loop: bounce"
-      />
-      <Button
-        @click="setPlayMode(PlayMode.Autoplay)"
-        :active="playMode === PlayMode.Autoplay"
-        icon="arrow_circle_right"
-        help="Autoplay"
-      />
-      <Button
-        @click="setPlayMode(PlayMode.Shuffle)"
-        :active="playMode === PlayMode.Shuffle"
-        icon="shuffle"
-        help="Autoplay random"
-      />
+    <div class="flex flex-row w-full">
+      <div class="flex-1 flex justify-start items-center gap-2">
+        <div>Cross section <em>w</em></div>
+        <div>=</div>
+        <input
+          class="bg-primary p-1 rounded-sm text-center"
+          type="number"
+          v-model.number="model"
+          min="-1"
+          max="1"
+          step="0.01"
+        />
+      </div>
+      <div class="flex-1 flex justify-center items-center gap-2">
+        <Button @click="goToZero" square text="0" help="W = 0" />
+        <Button
+          :active="playing"
+          @click="togglePlay"
+          icon="play_arrow"
+          help="Play"
+        />
+        <Button @click="pause" icon="pause" help="Pause" />
+      </div>
+      <div class="flex-1 flex justify-end items-center gap-2">
+        <Button
+          @click="setPlayMode(PlayMode.Loop)"
+          :active="playMode === PlayMode.Loop"
+          icon="laps"
+          help="Loop: wrap"
+        />
+        <Button
+          @click="setPlayMode(PlayMode.Zigzag)"
+          :active="playMode === PlayMode.Zigzag"
+          icon="turn_sharp_right"
+          help="Loop: bounce"
+        />
+        <Button
+          @click="setPlayMode(PlayMode.Autoplay)"
+          :active="playMode === PlayMode.Autoplay"
+          icon="arrow_circle_right"
+          help="Autoplay"
+        />
+        <Button
+          @click="setPlayMode(PlayMode.Shuffle)"
+          :active="playMode === PlayMode.Shuffle"
+          icon="shuffle"
+          help="Autoplay random"
+        />
+      </div>
     </div>
   </div>
 </template>
