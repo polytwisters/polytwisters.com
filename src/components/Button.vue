4c5e268f59438a7ade1a0f1f3a089f846f8aa6b2
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-10-08 19:36:46 -0700

    Remove tooltips from buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -29,15 +29,5 @@ defineProps<{
         {{ icon ? icon : text }}
       </div>
     </button>
-    <div
-      v-if="help"
-      class="not-group-hover:opacity-0 opacity-90 transition-opacity select-none pointer-events-none absolute bottom-10 min-w-20 w-fit bg-gray-700 p-1 rounded-sm text-sm text-center"
-    >
-      {{ help }}
-    </div>
-    <div
-      v-if="help"
-      class="not-group-hover:opacity-0 opacity-90 transition-opacity select-none pointer-events-none absolute bottom-9 box-content size-0 left-2.5 border-transparent border-6 border-b-0 border-t-gray-700 border-solid"
-    ></div>
   </div>
 </template>
