853b24ad6615615ce3bb7e3321468af180a95e29
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-11-02 01:21:53 -0800

    Update comment

--- src/components/HelpModal.vue
+++ src/components/HelpModal.vue
@@ -21,17 +21,22 @@ function onClose() {
 
 <template>
   <!--
-  For best accessibility, I use the HTML <dialog> element, and place the backdrop using a Teleport.
-  There are some serious weirdnesses with the layout in the top layer. In particular, I wanted
-  control over the layout, so I first tried setting the <dialog> element to full-screen so I could
-  place the actual dialog box using grid. But thanks to a bizarre subtle interaction between
-  Tailwind and the default user-agent styles, "fixed top-0 left-0 w-screen h-screen" creates a gap
-  to the right and bottom, which I spent many hours debugging. The solution would technically be
-  "max-w-screen max-h-screen" to override a user-agent setting, but I decided to give up with that
-  approach and just make the <dialog> and the dialog box the same.
+  It would be nice if I could create a full-size element and position the dialog box in the center
+  using standard grid methods. That would work fine, but for accessibility I wanted to also use
+  the <dialog> element, and there my troubles began.
 
-  Here the height is 100vh - 10rem and the width is between 50rem and 100vw - 10rem, and a standard
-  "translate" trick is used for horizontal centering.
+  There are some serious weirdnesses with the layout in the top layer. I first tried setting the
+  <dialog> element to full-screen so I could place the actual dialog box using grid. But thanks to a
+  really subtle interaction between Tailwind and the default user-agent styles,
+  "fixed top-0 left-0 w-screen h-screen" creates a 10rem gap to the right and bottom, which I spent
+  many hours debugging. The solution would technically be "max-w-screen max-h-screen" to override a
+  user-agent setting, but I decided to give up with that approach and just make the <dialog> and the
+  dialog box the same. It was a bit of an XY problem anyway -- the <dialog> shouldn't contain the
+  backdrop.
+
+  So I can't use CSS grid to center the dialog box in the viewport. Instead I set the height to
+  100vh - 10rem and the width between 50rem and 100vw - 10rem, and a standard "translate" trick is
+  used for horizontal centering.
   -->
   <dialog
     ref="dialog"
