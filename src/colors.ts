21eb7a28d813af58c87fb489a06adde04de9293b
Nathan Ho <nathan.waikin.ho@gmail.com>
2025-12-07 14:59:28 -0800

    Adjust colors

--- src/colors.ts
+++ src/colors.ts
@@ -1,12 +1,13 @@
 import { Fraction, fractionToString } from "./fraction";
 
 const colors = {
-  pink: "#e64980",
+  pink: "#fcc2d7",
   blue: "#339af0",
   white: "#ffffff",
   lightBlue: "#a5d8ff",
   yellow: "#ffec99",
-  orange: "#ff922b",
+  orange: "#ffd8a8",
+  lightGreen: "#d8f5a2",
   green: "#51cf66",
   purple: "#cc5de8",
   red: "#fa5252",
@@ -15,13 +16,13 @@ const colors = {
 const faceTypeColorMap: Map<string, string> = new Map();
 faceTypeColorMap.set("2", colors.white);
 faceTypeColorMap.set("3", colors.lightBlue);
-faceTypeColorMap.set("3/2", colors.yellow);
-faceTypeColorMap.set("4", colors.red);
+faceTypeColorMap.set("3/2", colors.blue);
+faceTypeColorMap.set("4", colors.lightGreen);
 faceTypeColorMap.set("4/3", colors.green);
-faceTypeColorMap.set("5", colors.orange);
-faceTypeColorMap.set("5/2", colors.blue);
-faceTypeColorMap.set("5/3", colors.pink);
-faceTypeColorMap.set("5/4", colors.purple);
+faceTypeColorMap.set("5", colors.red);
+faceTypeColorMap.set("5/2", colors.orange);
+faceTypeColorMap.set("5/3", colors.yellow);
+faceTypeColorMap.set("5/4", colors.pink);
 
 export function faceSymbolToColor(faceSymbol: Fraction): string {
   return faceTypeColorMap.get(fractionToString(faceSymbol)) ?? "white";
