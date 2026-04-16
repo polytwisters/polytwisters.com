72257b133d4158cbf29e4b615fd8645858579199
Nathan Ho <nathan.waikin.ho@gmail.com>
2026-04-15 21:28:34 -0700

    Change modal a bit

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -29,22 +29,8 @@ function close() {
 
 <template>
   <!--
-  It would be nice if I could create a full-size element and position the dialog box in the center
-  using standard grid methods. That would work fine, but for accessibility I wanted to also use
-  the <dialog> element, and there my troubles began.
-
-  There are some serious weirdnesses with the layout in the top layer. I first tried setting the
-  <dialog> element to full-screen so I could place the actual dialog box using grid. But thanks to a
-  really subtle interaction between Tailwind and the default user-agent styles,
-  "fixed top-0 left-0 w-screen h-screen" creates a 10rem gap to the right and bottom, which I spent
-  many hours debugging. The solution would technically be "max-w-screen max-h-screen" to override a
-  user-agent setting, but I decided to give up with that approach and just make the <dialog> and the
-  dialog box the same. It was a bit of an XY problem anyway -- the <dialog> shouldn't contain the
-  backdrop.
-
-  So I can't use CSS grid to center the dialog box in the viewport. Instead I set the height to
-  100vh - 10rem and the width between 100rem and 100vw - 10rem, and a standard "translate" trick is
-  used for horizontal centering.
+  The -translate-x-1/2 -translate-y-1/2 bits center the dialog horizontally and vertically. We
+  cannot use grid/flexbox here (believe me, I tried).
   -->
   <dialog
     ref="dialog"
@@ -54,7 +40,8 @@ function close() {
       max-h-[calc(100vh-5rem)] top-[50vh] -translate-y-1/2
       w-[50rem] max-w-[calc(100vw-5rem)] left-[50vw] -translate-x-1/2
       p-5
-      backdrop:bg-transparent theme-default:backdrop:backdrop-blur-sm
+      theme-default:backdrop:bg-transparent
+      theme-default:backdrop:backdrop-blur-sm
       bg-(--color-modal-bg) my-round my-shadow
     "
     @close="close"
